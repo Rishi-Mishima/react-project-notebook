22d5e93c193c43eb390bc00b85f8e37c022c4d98
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-12-20 23:55:03 +0800

    touch bar模版添加

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,9 +1,32 @@
-import { Outlet } from "react-router-dom"
-import { Button } from "antd-mobile"
+import { TabBar } from "antd-mobile"
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { Outlet } from "react-router-dom"
+import { useDispatch } from 'react-redux'
 import { getBillList } from "../../store/modules/billStore";
+import './index.scss'
+import {
+    BillOutline,
+    CalculatorOutline,
+    AddCircleOutline
+} from 'antd-mobile-icons'
 
+const tabs = [
+    {
+        key: '/month',
+        title: '月度账单',
+        icon: <BillOutline />,
+    },
+    {
+        key: '/new',
+        title: '记账',
+        icon: <AddCircleOutline />,
+    },
+    {
+        key: '/year',
+        title: '年度账单',
+        icon: <CalculatorOutline />,
+    },
+]
 
 const Layout = () => {
     const dispatch = useDispatch()
@@ -11,19 +34,19 @@ const Layout = () => {
         dispatch(getBillList())
     }, [dispatch])
     return (
-        <div>
-            <Outlet />
-            this is Layout
-
-            {/* 全局生效样式 */}
-            <Button color='primary'>global button</Button>
-
-            {/* 局部生效样式 */}
-            <div className="puple">
-                <Button color='success'>local button</Button>
+        <div className="layout">
+            <div className="container">
+                <Outlet />
+            </div>
+            <div className="footer">
+                <TabBar>
+                    {tabs.map(item => (
+                        <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                    ))}
+                </TabBar>
             </div>
         </div>
     )
 }
 
-export default Layout 
+export default Layout
