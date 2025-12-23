86950305add984d5e9220d40faa5b96cfb9a3375
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-12-23 21:48:15 +0800

    layout 界面

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,9 +1,69 @@
+// import { TabBar } from "antd-mobile"
+// import { useEffect } from "react"
+// import { Outlet, useNavigate } from "react-router-dom"
+// import { useDispatch } from 'react-redux'
+// import { getBillList } from "../../store/modules/billStore";
+// import './index.scss'
+// import {
+//     BillOutline,
+//     CalculatorOutline,
+//     AddCircleOutline
+// } from 'antd-mobile-icons'
+
+// const tabs = [
+//     {
+//         key: '/month',
+//         title: '月度账单',
+//         icon: <BillOutline />,
+//     },
+//     {
+//         key: '/new',
+//         title: '记账',
+//         icon: <AddCircleOutline />,
+//     },
+//     {
+//         key: '/year',
+//         title: '年度账单',
+//         icon: <CalculatorOutline />,
+//     },
+// ]
+
+// const Layout = () => {
+//     const dispatch = useDispatch()
+//     useEffect(() => {
+//         dispatch(getBillList())
+//     }, [dispatch])
+
+//     const navigate = useNavigate();
+//     const switchoRoute = (path) => {
+//         console.log(path)
+//         navigate(path)
+//     }
+//     return (
+//         <div className="layout">
+//             <div className="container">
+//                 <Outlet />
+//             </div>
+//             <div className="footer">
+//                 <TabBar onChange={switchoRoute}>
+//                     {tabs.map(item => (
+//                         <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+//                     ))}
+//                 </TabBar>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Layout
+
 import { TabBar } from "antd-mobile"
 import { useEffect } from "react"
-import { Outlet, useNavigate } from "react-router-dom"
+import { Outlet, useNavigate, useLocation } from "react-router-dom"
 import { useDispatch } from 'react-redux'
-import { getBillList } from "../../store/modules/billStore";
+import { getBillList } from "../../store/modules/billStore"
 import './index.scss'
+
 import {
     BillOutline,
     CalculatorOutline,
@@ -30,24 +90,36 @@ const tabs = [
 
 const Layout = () => {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    // 初始化获取全局账单数据
     useEffect(() => {
         dispatch(getBillList())
     }, [dispatch])
 
-    const navigate = useNavigate();
-    const switchoRoute = (path) => {
-        console.log(path)
-        navigate(path)
+    // TabBar 切换时跳转路由
+    const onTabChange = (key: string) => {
+        navigate(key)
     }
+
     return (
         <div className="layout">
             <div className="container">
                 <Outlet />
             </div>
+
             <div className="footer">
-                <TabBar onChange={switchoRoute}>
+                <TabBar
+                    activeKey={location.pathname}
+                    onChange={onTabChange}
+                >
                     {tabs.map(item => (
-                        <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                        <TabBar.Item
+                            key={item.key}
+                            icon={item.icon}
+                            title={item.title}
+                        />
                     ))}
                 </TabBar>
             </div>
