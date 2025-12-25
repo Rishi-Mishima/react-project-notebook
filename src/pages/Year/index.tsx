998a702217f060506a5e9e0509f2579bae2ee124
Rishi-Mishima <153814332+Rishi-Mishima@users.noreply.github.com>
2025-12-25 21:31:11 +0800

    update year

--- src/pages/Year/index.tsx
+++ src/pages/Year/index.tsx
@@ -1,10 +1,43 @@
+import { useSelector } from 'react-redux'
+import './index.scss'
+
 const Year = () => {
+    // 1️⃣ 从 Redux 拿到账单列表
+    const billList = useSelector((state: any) => state.bill.billList)
+
+    // 2️⃣ 年度统计
+    const totalPay = billList
+        .filter(item => item.type === 'pay')
+        .reduce((sum, item) => sum + item.amount, 0)
+
+    const totalIncome = billList
+        .filter(item => item.type === 'income')
+        .reduce((sum, item) => sum + item.amount, 0)
+
+    const balance = totalIncome - totalPay
 
     return (
-        <div>
-            this is Year
+        <div className="year">
+            <h2 className="title">年度账单</h2>
+
+            <div className="summary">
+                <div className="item">
+                    <span className="label">年度支出</span>
+                    <span className="value pay">￥{totalPay}</span>
+                </div>
+
+                <div className="item">
+                    <span className="label">年度收入</span>
+                    <span className="value income">￥{totalIncome}</span>
+                </div>
+
+                <div className="item">
+                    <span className="label">年度结余</span>
+                    <span className="value balance">￥{balance}</span>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Year 
+export default Year
